5dc1303628f1b6d512f6ca3d8f920e41abb9da50
Will <weiye.chen96@icloud.com>
2021-06-19 01:14:47 +0800

    finished reveal on scroll feature

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import '../styles/styles.css'
-import MobileMenu from '../scripts/MobileMenu.js'
+import MobileMenu from '../scripts/modules/MobileMenu.js'
+import ScrollReveal from '../scripts/modules/ScrollReveal.js'
 
 let mm = new MobileMenu;
+let scrollRevealFeatures = new ScrollReveal(document.querySelectorAll('.feature-item'), 0.25)
+let scrollRevealTestimonials = new ScrollReveal(document.querySelectorAll('.testimonial-item'), 0.25)
 
 if (module.hot) {
     module.hot.accept() //accept hot updates 
